3913a8b2054dab516d3d7365b8f708f3ca9cca11
opilane <opilane@HP-CZC6297B8F>
2021-11-12 12:20:43 +0200

    Bank pomogite

--- Omad_funktsioonid.py
+++ Omad_funktsioonid.py
@@ -1,7 +1,7 @@
 from modul import *
 while True:
     print("Funktsioonid".center(50,"+"))
-    print("arithmetic- A, \nis_year_leap-Y, \nsquare-N")
+    print("arithmetic- A, \nis_year_leap-Y, \nsquare-N, \nseason-S, \nKooderimine-X, \nnalik-M, \nis_prime-P, \ndate-D")
     v=input()
     if v.upper()=="A":
         arv1=float(input("Arv 1: "))
@@ -15,5 +15,27 @@ while True:
     elif v.upper()=="N":
         rezult=square(int(input("Sisestage piirkond: ")))
         print(rezult)
-
-            
+    elif v.upper()=="S":
+        rezult=season(int(input("Sisestage hooaeg: ")))
+        print(rezult)
+    elif v.upper()=="X":
+        print("Kooderimine".center(60,"*"))
+        rezult=xor_uncipher(input("sisesta text: "), input("Sisesta võti: "))
+        print(rezult)
+        print("Dekodeerimine".center(60,"*"))
+        de_rezult=xor_uncipher(rezult, input("Sisesta võti: "))
+        print(de_rezult)
+    elif v.upper()=="M":
+        a=float(input("Sisestage sissemakse summa: "))
+        years=int(input("Mitu aastat on möödunud?"))
+        rezult=nal(a,years)
+        print(rezult)
+    elif v.upper()=="P":
+        rezult=is_prime(int(input("kirjutage arv vahemikus (1-1000): ")))
+        print(rezult)
+    elif v.upper()=="D":
+        day=int(input("Sisestage päev: "))
+        month=int(input("Sisestage kuu: "))
+        year=int(input("Sisestage aasta: "))
+        result=date(day,month,year)
+        print(result) 
--- modul.py
+++ modul.py
@@ -38,6 +38,7 @@ def is_year_leap(aasta: int):
     return vastus 
 
 def square(a):
+#Kirjutage ruudu üks külg ja programm arvutab korraga pindala, perimeetri ja dioganaali
     P=4*a
     S=a*a
     D=(a**2)/2
@@ -48,7 +49,10 @@ def square(a):
     return K
 
 def season(sea):
+#kirjuta number 1-12 ja programm arvutab välja, mis aastaaeg on
    if sea == 12 or 1 <= sea <= 2:
+       print("Talv")
+   elif 0<kuu<3:
        print("Talv")
    elif  3 <= sea <= 5:
        print("Kevad")
@@ -58,6 +62,55 @@ def season(sea):
        print("sügis")
    else:
        print("Vale süntaks")
-        n = int(input("Sisestage kuu number (1-12): "))
+   return sea
 
-        season(n)
+def xor_uncipher(string:str, key: str)->str:
+#Kodeeritud text dekodeeritakse
+    result = ''
+    temp = []
+    for i in range(len(string)):
+        temp.append(string[i])
+        for j in reversed(range(len(key))):
+            temp[i] = chr(ord(key[j]) ^ ord(temp[i]))
+        result += temp[i]
+    return result
+
+def nal(a, e, y):
+#Kasutaja teeb aastateks euro sissemakse 10% aastas ja saab igal aastal, näiteks 1 = 1,10
+    a = int(input())
+    e = int(input())
+    y = int(input())
+
+    nal = a
+    year = y
+    def money():
+            if year >0:
+                nal = a*1.1+e
+                year = year -1
+                return money()
+            else:
+                return nal
+
+def is_prime(b):
+#Kirjutame arvu vahemikus 1-1000 ja programm kirjutab selle arvu tõeseks või mitte (True or False)
+    k=2
+    while k*k<=b:
+        if b%k==0:
+            return False
+        k+=1
+    return True
+
+def date(d,m,y):
+#kirjutamispäev näiteks 26
+#kirjuta näiteks kuu 6
+#kirjuta näiteks aasta 2020
+#Selle tulemusena saame õige kuupäeva või mitte(True or False)
+    if d<0 or m<0 or y<0:
+        return False
+    mon=[31,28,31,30,31,30,31,31,30,31,30,31]
+    if y%400==0 or ((y%4==0) and (y%100 != 0)):
+        mon[1]=29
+    if m>=1 and m<=12:
+       if d>0 and d<=mon[m-1]:
+           return True
+    return False
